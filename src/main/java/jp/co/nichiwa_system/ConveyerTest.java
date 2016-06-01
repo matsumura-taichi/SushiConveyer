de838043a25712ae6a310ca8ccf912f9828501a7
matsumura-taichi <matsumura.taichi@nichiwa-system.co.jp>
2016-06-01 13:41:26 +0900

    ネタ配列にまぐろを追加して取得、緑になった

--- src/main/java/jp/co/nichiwa_system/ConveyerTest.java
+++ src/main/java/jp/co/nichiwa_system/ConveyerTest.java
@@ -9,7 +9,10 @@ public class ConveyerTest {
 	@Test
 	public void testFewNetaList1() {
 		Conveyer conveyer = new Conveyer();
+		conveyer.add("まぐろ");
 		String[] fewNetas = conveyer.getFewNetaList();
+
+		assertEquals(1, fewNetas.length);
 		assertEquals("まぐろ", fewNetas[0]);
 	}
 
--- src/test/java/jp/co/nichiwa_system/Conveyer.java
+++ src/test/java/jp/co/nichiwa_system/Conveyer.java
@@ -2,10 +2,15 @@ package jp.co.nichiwa_system;
 
 public class Conveyer {
 
+	private  String[] netas;
+
 	public String[] getFewNetaList() {
-		String[] netas = new String[1];
-		netas[0] = "まぐろ";
 		return netas;
+	}
+
+	public void add(String neta) {
+		netas = new String[1];
+		netas[0] = neta;
 	}
 
 }
